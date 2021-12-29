71e3120df1b7e9d9120d01a1dcaef1fa0ac2633b
Santanu Dey <santanu.dey84@yahoo.in>
2021-12-29 07:30:34 +0530

    Interfaces

---
+++ Interfaces/src/portfolio/javamasterclass/ISaveable.java
@@ -0,0 +1,8 @@
+package portfolio.javamasterclass;
+
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+}
---
+++ Interfaces/src/portfolio/javamasterclass/Main.java
@@ -0,0 +1,230 @@
+package portfolio.javamasterclass;
+
+import java.util.*;
+
+public class Main {
+
+    private static final List<Album> albums = new ArrayList<>();
+
+    public static void main1(String[] args) {
+        ITelephone myPhone;
+        myPhone = new DeskPhone(123456);
+        myPhone.powerOn();
+        myPhone.callPhone(123456);
+        myPhone.answer();
+
+        myPhone = new MobilePhone(234567);
+        //myPhone.powerOn();
+        myPhone.callPhone(234567);
+        myPhone.answer();
+    }
+
+    public static void main2(String[] args) {
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy Man", 4.3);
+        album.addSong("Hold On", 5.6);
+        album.addSong("Lady Double Dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High Ball Shooter", 4.27);
+        album.addSong("The Gypsy", 4.2);
+        album.addSong("Soldier of Fortune", 3.13);
+        albums.add(album);
+
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Let's go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D.", 5.25);
+        album.addSong("Breaking the rules", 5.32);
+        album.addSong("Night of the long knives", 5.12);
+        albums.add(album);
+
+        List<Song> playList = new ArrayList<>();
+        //List<Song> playList = new Vector<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy Man", playList);
+        albums.get(0).addToPlayList("Speed King", playList);  // Does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(8, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);  // Does not exist
+
+        play(playList);
+    }
+
+    public static void main(String[] args) {
+        Player santanu = new Player("Santanu", 10, 15);
+        System.out.println(santanu.toString());
+        saveObject(santanu);
+
+        santanu.setHitPoints(8);
+        System.out.println(santanu);
+        santanu.setWeapon("Stormbringer");
+        saveObject(santanu);
+        //loadObject(santanu);
+        System.out.println(santanu);
+
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println("Strength = " + ((Monster) werewolf).getStrength());
+        //System.out.println(werewolf);
+        saveObject(werewolf);
+    }
+
+    private static void play(List<Song> playList) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
+        ListIterator<Song> listIterator = playList.listIterator();
+        if (playList.size() == 0) {
+            System.out.println("No songs in playlist");
+            return;
+        } else {
+            System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Playlist complete");
+                    quit = true;
+                    break;
+
+                case 1:
+                    if (!forward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    } else {
+                        System.out.println("We've reached the end of the playlist");
+                        forward = false;
+                    }
+                    break;
+                case 2:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        forward = true;
+                    }
+                    break;
+                case 3:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We're at the start of the list");
+                        }
+                    } else {
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+                case 6:
+                    if (playList.size() > 0) {
+                        listIterator.remove();
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid action");
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\npress");
+        System.out.println("0 - to quit\n" +
+                "1 - to play next song\n" +
+                "2 - to play previous song\n" +
+                "3 - to replay the current song\n" +
+                "4 - list songs in the playlist\n" +
+                "5 - print available actions\n" +
+                "6 - delete current song from playlist");
+    }
+
+    private static void printList(List<Song> playList) {
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("=======================");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+        }
+        System.out.println("=======================");
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+                default:
+                    System.out.println("Wrong choice input");
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+}
