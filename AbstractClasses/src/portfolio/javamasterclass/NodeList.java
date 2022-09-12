9cd600e76c21ff33e6c1af9d2c3cf5aca45dcb23
Santanu Dey <santanu.dey84@yahoo.in>
2022-09-12 22:41:21 +0530

    Abstract Classes - Linked List and BST

---
+++ AbstractClasses/src/portfolio/javamasterclass/NodeList.java
@@ -0,0 +1,8 @@
+package portfolio.javamasterclass;
+
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem newItem);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
