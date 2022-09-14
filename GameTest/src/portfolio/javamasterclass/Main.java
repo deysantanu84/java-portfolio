b3f0d36fddb1df113c49a7773e59be2a0f3b15c8
Santanu Dey <santanu.dey84@yahoo.in>
2022-09-14 22:00:30 +0530

    Packages

---
+++ GameTest/src/portfolio/javamasterclass/Main.java
@@ -0,0 +1,82 @@
+package portfolio.javamasterclass;
+
+import com.example.game.ISaveable;
+import com.example.game.Monster;
+import com.example.game.Player;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player santanu = new Player("Santanu", 10, 15);
+        System.out.println(santanu.toString());
+        saveObject(santanu);
+
+        santanu.setHitPoints(8);
+        System.out.println(santanu);
+        santanu.setWeapon("Stormbringer");
+        saveObject(santanu);
+        //loadObject(santanu);
+        System.out.println(santanu);
+
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println("Strength = " + ((Monster) werewolf).getStrength());
+        //System.out.println(werewolf);
+        saveObject(werewolf);
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\npress");
+        System.out.println("0 - to quit\n" +
+                "1 - to play next song\n" +
+                "2 - to play previous song\n" +
+                "3 - to replay the current song\n" +
+                "4 - list songs in the playlist\n" +
+                "5 - print available actions\n" +
+                "6 - delete current song from playlist");
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+                default:
+                    System.out.println("Wrong choice input");
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+}
