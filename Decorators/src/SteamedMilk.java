e2d7ef1981920cae6ccf47790ab26e0321fbfd54
deysantanu84 <santanu.dey84@yahoo.in>
2022-09-20 18:45:22 +0530

    Decorators

---
+++ Decorators/src/SteamedMilk.java
@@ -0,0 +1,17 @@
+public class SteamedMilk extends CondimentDecorator {
+    Beverage beverage;
+
+    public SteamedMilk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Steamed Milk";
+    }
+}
